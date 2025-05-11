0cd49fc80e10aa0965096d852495cb30f6a6dc91
Akila201 <akiakiayla@gmail.com>
2025-05-11 23:17:35 +0530

    routing issue fixed

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -16,4 +16,18 @@ axiosInstance.interceptors.request.use((config) => {
     return Promise.reject(error);
 });
 
+axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response && error.response.status === 401) {
+            const currentPath = window.location.pathname;
+            if (currentPath !== '/login' && currentPath !== '/register') {
+                localStorage.removeItem('authToken'); 
+                window.location.href = '/login';
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default axiosInstance;
